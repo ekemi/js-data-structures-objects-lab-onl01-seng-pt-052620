6e297de7ed9ae78fdd10caeb56575b972d778f3f
edmundo papi <edmundopierre@yahoo.com>
2020-11-29 10:57:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-var recipes = {}
-function updateDriverWithKeyAndValue(object,key,value){
-  return Object.assign({},object,{[key]: value})
+var drive = {}
+function updateDriverWithKeyAndValue(driver,key,value){
+  return Driver.assign({},driver,{[key]: value})
 }
