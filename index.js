93264e782bd3130bba3f617798b26d7ac1dca475
edmundo papi <edmundopierre@yahoo.com>
2020-11-29 11:16:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 function deleteFromDriverByKey(driver, key) {
   var newDriver = Object.assign({}, driver);
   delete newDriver.key;
-  return newDriver;
+  return driver;
 }
 function destructivelyDeleteFromDriverByKey(driver, key){
   delete driver.key;
