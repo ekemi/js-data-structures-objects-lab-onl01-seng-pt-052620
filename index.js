715de02752937a30e9637485b5f91cb891a599be
edmundo papi <edmundopierre@yahoo.com>
2020-11-29 11:07:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var driver = {}
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value })
 }
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+  return driver;
+}
