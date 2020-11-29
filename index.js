a388794558ac88edc1dd54ce4f57705c33dc1f06
edmundo papi <edmundopierre@yahoo.com>
2020-11-29 11:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function deleteFromDriverByKey(driver, key) {
   return newDriver;
 }
 function destructivelyDeleteFromDriverByKey(driver, key){
-  delete driver.key;
+  delete driver[key];
   return driver;
 }
